a34fce7ee210c3088dd7cbd7200bd4a9aea3070d
Richard Brattlund <info@richardbrattlund.se>
2014-05-17 14:40:21 +0200

    Räkna ut procent mot viewport-storlek

--- app/js/app-scripts.js
+++ app/js/app-scripts.js
@@ -1,3 +1,33 @@
-$(window).click(function(e) {
-	console.log(e.pageX+ ' , ' + e.pageY);
-});
+
+
+//////////////////////////
+// Variabler
+//////////////////////////
+
+	var xPosPercent, yPosPercent;
+	var xViewport = $(window).width();
+	var yViewport = $(window).height();
+
+
+//////////////////////////
+// Starta allt
+//////////////////////////
+
+	$(window).click(function(e) {
+		var xPos = e.pageX;
+		var yPos = e.pageY;
+
+		xPosPercent = getPosPercent(xPos, xViewport);
+		yPosPercent = getPosPercent(yPos, yViewport)
+		console.log(xPosPercent + ', ' + yPosPercent);
+	});
+
+
+//////////////////////////
+// Funktioner
+//////////////////////////
+
+	function getPosPercent(pos, viewport) {
+		return Math.floor((pos / viewport) * 100);
+	}
+
